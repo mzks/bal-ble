6cd47ecfd62f7f5a0af7fe4cf4b48bf196a6581b
Keita Mizukoshi <mizukoshi.keita@jaxa.jp>
2024-02-02 17:19:54 +0900

    Initial application test

--- central/central.py
+++ central/central.py
@@ -1,11 +1,8 @@
-"""
-UART Service
--------------
+# central handling script
+#
 
-An example showing how to write a simple program using the Nordic Semiconductor
-(nRF) UART service.
-
-"""
+import argparse
+from datetime import datetime
 
 import asyncio
 import sys
@@ -24,18 +21,10 @@ UART_TX_CHAR_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
 # TIP: you can get this function and more from the ``more-itertools`` package.
 def sliced(data: bytes, n: int) -> Iterator[bytes]:
-    """
-    Slices *data* into chunks of size *n*. The last slice may be smaller than
-    *n*.
-    """
     return takewhile(len, (data[i : i + n] for i in count(0, n)))
 
 
-async def uart_terminal():
-    """This is a simple "terminal" program that uses the Nordic Semiconductor
-    (nRF) UART service. It reads from stdin and sends each line of data to the
-    remote device. Any data received from the device is printed to stdout.
-    """
+async def uart_terminal(args=None):
 
     def match_nus_uuid(device: BLEDevice, adv: AdvertisementData):
         # This assumes that the device includes the UART service UUID in the
@@ -49,44 +38,40 @@ async def uart_terminal():
     device = await BleakScanner.find_device_by_filter(match_nus_uuid)
 
     if device is None:
-        print("no matching device found, you may need to edit match_nus_uuid().")
+        print("no matching device found, you may need to UUID.")
         sys.exit(1)
 
     def handle_disconnect(_: BleakClient):
         print("Device was disconnected, goodbye.")
-        # cancelling all tasks effectively ends the program
         for task in asyncio.all_tasks():
             task.cancel()
 
     def handle_rx(_: BleakGATTCharacteristic, data: bytearray):
-        print("received:", data)
+
+        if args.filename:
+            with open(args.filename, mode='a') as f:
+                if args.time:
+                    f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f, "))
+                f.write(data)
+
+        if args.echo:
+            print("Rx:", data)
 
     async with BleakClient(device, disconnected_callback=handle_disconnect) as client:
         await client.start_notify(UART_TX_CHAR_UUID, handle_rx)
 
-        print("Connected, start typing and press ENTER...")
+        print("Connected, start typing command (if defined) and press ENTER...")
 
         loop = asyncio.get_running_loop()
         nus = client.services.get_service(UART_SERVICE_UUID)
         rx_char = nus.get_characteristic(UART_RX_CHAR_UUID)
 
         while True:
-            # This waits until you type a line and press ENTER.
-            # A real terminal program might put stdin in raw mode so that things
-            # like CTRL+C get passed to the remote device.
             data = await loop.run_in_executor(None, sys.stdin.buffer.readline)
 
             # data will be empty on EOF (e.g. CTRL+D on *nix)
             if not data:
                 break
-
-            # some devices, like devices running MicroPython, expect Windows
-            # line endings (uncomment line below if needed)
-            # data = data.replace(b"\n", b"\r\n")
-
-            # Writing without response requires that the data can fit in a
-            # single BLE packet. We can use the max_write_without_response_size
-            # property to split the data into chunks that will fit.
 
             for s in sliced(data, rx_char.max_write_without_response_size):
                 await client.write_gatt_char(rx_char, s, response=False)
@@ -95,8 +80,16 @@ async def uart_terminal():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            prog='central',
+            description='bal-ble central script')
+    parser.add_argument('-f', '--filename', default=None, help='Filename to save the recieved data')
+    parser.add_argument('-e', '--echo', action='store_true', help='Show recieved data on terminal')
+    parser.add_argument('-t', '--time', action='store_true', help='Append datetime for the recieved data')
+    args = parser.parse_args()
+
     try:
-        asyncio.run(uart_terminal())
+        asyncio.run(uart_terminal(args))
     except asyncio.CancelledError:
         # task is cancelled on disconnect, so we ignore this error
         pass
--- peripheral/peripheral.ino
+++ peripheral/peripheral.ino
@@ -1,6 +1,5 @@
 /*********************************************************************
 This is an example on a peripheral device
-
 *********************************************************************/
 
 #include <bluefruit.h>
@@ -18,90 +17,54 @@ void setup()
   Serial.begin(115200);
 
 #if CFG_DEBUG
-  // Blocking wait for connection when debug mode is enabled via IDE
   while ( !Serial ) yield();
 #endif
-  
-  Serial.println("Bluefruit52 BLEUART Example");
-  Serial.println("---------------------------\n");
 
-  // Setup the BLE LED to be enabled on CONNECT
-  // Note: This is actually the default behavior, but provided
-  // here in case you want to control this LED manually via PIN 19
   Bluefruit.autoConnLed(true);
-
-  // Config the peripheral connection with maximum bandwidth 
-  // more SRAM required by SoftDevice
-  // Note: All config***() function must be called before begin()
   Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
-
   Bluefruit.begin();
-  Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
+  Bluefruit.setTxPower(4); // nRF52840: -40dBm, -20, -16, -12, -8, -4, 0, 2, 3, 4, 5, 6, 7 and +8dBm are supported.
+
   //Bluefruit.setName(getMcuUniqueID()); // useful testing with multiple central connections
   Bluefruit.Periph.setConnectCallback(connect_callback);
   Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
 
-  // To be consistent OTA DFU should be added first if it exists
   bledfu.begin();
-
-  // Configure and Start Device Information Service
-  bledis.setManufacturer("Adafruit Industries");
-  bledis.setModel("Bluefruit Feather52");
+  bledis.setManufacturer("Manifucture nane");
+  bledis.setModel("Bal-ble board");
   bledis.begin();
 
-  // Configure and Start BLE Uart Service
   bleuart.begin();
-
-  // Start BLE Battery Service
   blebas.begin();
-  blebas.write(100);
+  blebas.write(100);  // dummy
 
-  // Set up and start advertising
   startAdv();
-
-  Serial.println("Please use Adafruit's Bluefruit LE app to connect in UART mode");
-  Serial.println("Once connected, enter character(s) that you wish to send");
 }
 
 void startAdv(void)
 {
-  // Advertising packet
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
-
-  // Include bleuart 128-bit uuid
   Bluefruit.Advertising.addService(bleuart);
-
-  // Secondary Scan Response packet (optional)
-  // Since there is no room for 'Name' in Advertising packet
   Bluefruit.ScanResponse.addName();
-  
-  /* Start Advertising
-   * - Enable auto advertising if disconnected
-   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
-   * - Timeout for fast mode is 30 seconds
-   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
-   * 
-   * For recommended advertising interval
-   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
-   */
   Bluefruit.Advertising.restartOnDisconnect(true);
   Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
   Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
   Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
 }
 
+bool data_sending = true;
+
 void loop()
 {
   // Forward data from HW Serial to BLEUART
-  while (Serial.available())
+  while (data_sending)
   {
     // Delay to wait for enough input, since we have a limited transmission buffer
     delay(2);
 
-    uint8_t buf[64];
-    int count = Serial.readBytes(buf, sizeof(buf));
-    bleuart.write( buf, count );
+    char tx_string[] = "0001,0002,0003,0004,0005\n"
+    bleuart.write( tx_string, strlen(tx_string) );
   }
 
   // Forward from BLEUART to HW Serial
@@ -109,7 +72,10 @@ void loop()
   {
     uint8_t ch;
     ch = (uint8_t) bleuart.read();
-    Serial.write(ch);
+    if (ch == "stop")
+        data_sending = false;
+    if (ch == "start")
+        data_sending = true;
   }
 }
 
@@ -126,11 +92,6 @@ void connect_callback(uint16_t conn_handle)
   Serial.println(central_name);
 }
 
-/**
- * Callback invoked when a connection is dropped
- * @param conn_handle connection where this event happens
- * @param reason is a BLE_HCI_STATUS_CODE which can be found in ble_hci.h
- */
 void disconnect_callback(uint16_t conn_handle, uint8_t reason)
 {
   (void) conn_handle;
