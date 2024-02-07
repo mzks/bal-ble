1647d0632e94a06bcbf9c78f34e6a676063d5ad6
Keita Mizukoshi <mizukoshi.keita@jaxa.jp>
2024-02-07 15:31:08 +0900

    add battery level sensor and i2c devices

--- peripheral/peripheral.ino
+++ peripheral/peripheral.ino
@@ -18,12 +18,22 @@ BLEDis  bledis;  // device information
 BLEUart bleuart; // uart over ble
 BLEBas  blebas;  // battery
 
+// --   User should edit here   --
+// include and global variables
+#include <Tle493d_w2b6.h>
+Tle493d_w2b6 Tle493dMagnetic3DSensor = Tle493d_w2b6(Tle493d_w2b6::MASTERCONTROLLEDMODE, Tle493d_w2b6::TLE493D_W2B6_A1);
+
+
+#define HICHG 22
+// -------------------------------
+
+
 void setup(){
 
   Bluefruit.autoConnLed(true);
   Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
   Bluefruit.begin();
-  Bluefruit.setTxPower(4); // nRF52840: -40dBm, -20, -16, -12, -8, -4, 0, 2, 3, 4, 5, 6, 7 and +8dBm are supported.
+  Bluefruit.setTxPower(8); // nRF52840: -40dBm, -20, -16, -12, -8, -4, 0, 2, 3, 4, 5, 6, 7 and +8dBm are supported.
 
   //Bluefruit.setName(getMcuUniqueID()); // useful testing with multiple central connections
   Bluefruit.Periph.setConnectCallback(connect_callback);
@@ -41,8 +51,15 @@ void setup(){
   startAdv();
   // --   User should edit here   --
   // write actual sensor setup
+  Tle493dMagnetic3DSensor.begin();
+  Tle493dMagnetic3DSensor.enableTemp();
 
 
+  pinMode(HICHG, LOW); // high charging current
+  analogReference(AR_INTERNAL_2_4);
+  analogReadResolution(10);
+  pinMode(VBAT_ENABLE, OUTPUT);
+  digitalWrite(VBAT_ENABLE, LOW);
   // -------------------------------
 
 }
@@ -59,6 +76,10 @@ void startAdv(void)
   Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
 }
 
+int get_battery_level(){
+  int vbat_raw = analogRead(PIN_VBAT);
+  return vbat_raw;
+}
 
 int sleep_time = 1000;  // unit : ms
 bool data_sending = false;  // default false
@@ -68,11 +89,19 @@ void loop()
   // Send data
   if (data_sending)
   {
-    // --   User should edit here   --
-    // write actual sensor value(s)
-    char tx_string[] = "0001,0002,0003,0004,0005\n";
-    // -------------------------------
-    bleuart.write( tx_string, strlen(tx_string) );
+      // --   User should edit here   --
+      // write actual sensor value(s)
+      Tle493dMagnetic3DSensor.updateData();
+      float x = Tle493dMagnetic3DSensor.getX();
+      float y = Tle493dMagnetic3DSensor.getY();
+      float z = Tle493dMagnetic3DSensor.getZ();
+      int bat = get_battery_level();
+
+      String send_text = String(x) + "," + String(y) + "," + String(z) + "," + String(bat) + "\n";
+      char tx_string[40];
+      send_text.toCharArray(tx_string, 40);
+      // -------------------------------
+      bleuart.write( tx_string, strlen(tx_string) );
   }
 
 
@@ -89,8 +118,8 @@ void loop()
   // define your command here
   if (command == "stop\n") data_sending = false;
   if (command == "start\n") data_sending = true;
-  if (command == "fast\n") sleep_time = 1000;
-  if (command == "slow\n") sleep_time = 10000;
+  if (command == "fast\n") sleep_time = 50;
+  if (command == "slow\n") sleep_time = 1000;
 
 
   // -------------------------------
